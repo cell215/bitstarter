26ba8041d36674fdbd6be0c227a913594b83349b
cell215 <cell215@gmail.com>
2013-07-11 06:12:08 +0000

    Updated to not depend on local index.html file

--- grader.js
+++ grader.js
@@ -25,9 +25,7 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://vast-ridge-8973.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +34,6 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
-};
-
-var assertURLExists = function(inURL) {
-    var result;
-    rest.get(inURL).on('complete', function(result) {
-	if (result instanceof Error) {
-	    console.log('Error: ' + result.message);
-	}
-    });
-    return result;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -64,8 +52,29 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
     return out;
 };
+
+var checkURLFile = function(htmlfile, checksfile) {
+    rest.get(htmlfile).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error: ' + result.message);
+        }
+	var checks = loadChecks(checksfile).sort();
+    $ =  cheerio.load(result);
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+
+    });
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -76,12 +85,16 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_deployed>', 'Path to url to verifiy', clone(assertURLExists), URL_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url_deployed>', 'Path to url to verifiy')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if (program.url) {
+	checkJson = checkURLFile(program.url, program.checks);
+    }	
+    else {
+	checkJson = checkHtmlFile(program.file, program.checks);
+    }	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
